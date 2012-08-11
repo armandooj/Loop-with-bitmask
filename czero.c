8f67d6a336691c761116eb481be50368af3ab753
Grégory Mounié <Gregory.Mounie@imag.fr>
2012-08-11 12:19:04 +0200

    deplacement des fonctions en c

---
+++ czero.c
@@ -0,0 +1,23 @@
+#include "czero.c"
+
+void avecRestrictIf(const struct TripletD const * restrict tab,
+		    const char const * restrict bitmasque,
+		    int nb, struct TripletD * restrict somme) {
+  for(int i=0; i < nb; i++) {
+    if (bitmasque[i]) {
+      somme->valeurs[0] += tableau[i].valeurs[0]; 
+      //somme.valeurs[1] += tableau[i].valeurs[1]; 
+      //somme.valeurs[2] += tableau[i].valeurs[2];
+    }
+  } 
+}
+
+void avecRestrictZero(const struct TripletD const * restrict tab,
+		    const char const * restrict bitmasque,
+		    int nb, struct TripletD * restrict somme) {
+  for(int i=0; i < nb; i++) {
+      somme->valeurs[0] += bitmasque[i] * tableau[i].valeurs[0]; 
+      //somme.valeurs[1] += tableau[i].valeurs[1]; 
+      //somme.valeurs[2] += tableau[i].valeurs[2];
+  } 
+}
---
+++ czero.h
@@ -0,0 +1,9 @@
+
+
+void avecRestrictIf(const struct TripletD const * restrict tab,
+		    const char const * restrict bitmasque,
+		    int nb, struct TripletD * restrict somme);
+
+void avecRestrictZero(const struct TripletD const * restrict tab,
+		    const char const * restrict bitmasque,
+		    int nb, struct TripletD * restrict somme)
